23599814bbdddd979d360306da83c6e5a475a36d
Paulo D. Souza <subtosilencio@users.noreply.github.com>
2020-01-27 15:35:17 +0000

    Update testTravis.py

--- testTravis.py
+++ testTravis.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
 
-def main():
-    pass
+import unittest
+import os
+import requests
+import warnings
+
+def ignore_warnings(test_func):
+    def do_test(self, *args, **kwargs):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", ResourceWarning)
+            test_func(self, *args, **kwargs)
+    return do_test
+
+class TestPassword(unittest.TestCase):
+    '''A class to represent the model'''
+    
+    def testFile(self):
+        self.assertTrue(os.path.isfile('geckodriver'))
+        self.assertTrue(os.path.isfile('tempMailList.cvs'))
+    
+    @ignore_warnings
+    def testOnline(self):
+        self.assertEqual(str(requests.get('https://temp-mail.org/en/change')), '<Response [200]>')
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
